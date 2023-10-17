dce972d436fae8de76d330cf06cadbd6b9c5b564
M Akhlak <md.22scse1010882@galgotiasuniversity.edu.in>
2023-10-17 23:45:31 +0530

    Column major using c++

--- DSA/Matrices/LowerTriangle.cpp
+++ DSA/Matrices/LowerTriangle.cpp
@@ -61,13 +61,71 @@ public:
     }
 };
 
-
 // Coulmon major formula
 
+class ColumnMajorFormula
+{
+private:
+    int *B;
+    int x;
 
+public:
+    ColumnMajorFormula()
+    {
+        x = 2;
+        B = new int[2 * (2 + 1) / 2];
+    }
+    ColumnMajorFormula(int x)
+    {
+        this->x = x;
+        B = new int[x * (x + 1) / 2];
+    }
+    ~ColumnMajorFormula()
+    {
+        delete[] B;
+    }
+
+    void columnSet(int i, int j, int k)
+    {
+        if (i >= j)
+        {
+            B[x * (j - 1) - (j - 2) * (j - 1) / 2 + i - j] = k;
+        }
+    }
+    int columnGet(int i, int j)
+    {
+        if (i >= j)
+        {
+            return B[x * (j - 1) - (j - 2) * (j - 1) / 2 + i - j];
+        }
+    }
+    void columnMajorDisplay()
+    {
+        for (int i = 1; i <= x; i++)
+        {
+            for (int j = 1; j <= x; j++)
+            {
+                if (i >= j)
+                {
+                    cout << B[x * (j - 1) - (j - 2) * (j - 1) / 2 + i - j] << " ";
+                }
+                else
+                {
+                    cout << "0";
+                }
+            }
+            cout << endl;
+        }
+    }
+    int columnGetDimetion()
+    {
+        return x;
+    }
+};
 
 int main()
 {
+    cout << "Display by row major formula" << endl;
     int dim;
     cout << "Enter dimensions : ";
     cin >> dim;
@@ -86,6 +144,29 @@ int main()
     }
 
     mat.Display();
+
+    cout << endl;
+
+    cout << "Displaying by Column major formula" << endl;
+
+    int colDim;
+    cout << "Enter column dimension : ";
+    cin >> colDim;
+
+    ColumnMajorFormula colMat(colDim);
+
+    int colElement;
+    cout << "Enter column element : ";
+    for (int i = 1; i <= colDim; i++)
+    {
+        for (int j = 1; j <= colDim; j++)
+        {
+            cin >> colElement;
+            colMat.columnSet(i, j, colElement);
+        }
+    }
+
+    colMat.columnMajorDisplay();
 
     return 0;
 }

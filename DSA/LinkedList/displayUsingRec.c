b3534056264baeebdd0822dabd1c2143004e9467
M Akhlak <md.22scse1010882@galgotiasuniversity.edu.in>
2023-10-24 16:37:52 +0530

    learned to display Linked List in using  recurive and iterative

---
+++ DSA/LinkedList/displayUsingRec.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node
+{
+    int data;
+    struct Node *next;
+
+}*first = NULL;
+
+
+
+void create(int A[], int n)
+{
+    int i;
+    struct Node *t, *last;
+    first = (struct Node *)malloc(sizeof(struct Node));
+
+    first->data = A[0];
+    first->next = NULL;
+    last = first;
+
+    for (i = 1; i < n; i++)
+    {
+        t = (struct Node *)malloc(sizeof(struct Node));
+        t->data = A[i];
+        t->next = NULL;
+        last->next = t;
+        last = t;
+    }
+}
+
+void recusiveDisplay(struct Node *p)
+{
+    if (p != NULL)
+    {
+        printf("%d ", p->data);
+        recusiveDisplay(p->next);
+    }
+}
+// time complexity : o(n);
+// space complexity : o(n)
+
+void recusiveDisplayFirst(struct Node *p)
+{
+    if (p != NULL)
+    {
+        recusiveDisplayFirst(p->next);
+        printf("%d ", p->data);
+    }
+}
+// time complexity : o(n);
+// space complexity : o(n)
+
+
+int main()
+{
+
+    int A[] = {2,12,4,5,7,56,12,4,6};
+
+    create(A, 9);
+    recusiveDisplay(first); // display in normal order
+    printf("\n");
+    recusiveDisplayFirst(first); // Display in reverse order
+
+    return 0;
+}

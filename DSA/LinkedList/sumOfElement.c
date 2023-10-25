df0390dd1a76c367eb194388838a33a3619fe302
M Akhlak <md.22scse1010882@galgotiasuniversity.edu.in>
2023-10-26 00:24:44 +0530

    Learned to find sum of element in linked list

---
+++ DSA/LinkedList/sumOfElement.c
@@ -0,0 +1,81 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node
+{
+    int data;
+    struct Node *next;
+
+} *first = NULL;
+
+void create(int A[], int n)
+{
+    int i;
+    struct Node *t, *last;
+    first = (struct Node *)malloc(sizeof(struct Node));
+
+    first->data = A[0];
+    first->next = NULL;
+
+    last = first;
+
+    for (i = 1; i < n; i++)
+    {
+        t = (struct Node *)malloc(sizeof(struct Node));
+        t->data = A[i];
+        t->next = NULL;
+        last->next = t;
+        last = t;
+    }
+}
+
+void display(struct Node *p)
+{
+    while (p != NULL)
+    {
+        printf("%d ", p->data);
+        p = p->next;
+    }
+}
+
+int sumOfLinkedList(struct Node *p)
+{
+    int sum = 0;
+    while (p != NULL)
+    {
+        sum += p->data;
+        p = p->next;
+    }
+    return sum;
+}
+
+// Recursive function for sum of linked lists
+int recursiveSumOfLinkedList(struct Node *p)
+{
+    if (p == NULL)
+    {
+        return 0;
+    }
+    else
+    {
+        return recursiveSumOfLinkedList(p->next) + p->data;
+    }
+}
+
+int main()
+{
+
+    int A[] = {3, 5, 7, 10, 25, 8, 32, 2};
+    create(A, 8);
+    printf("Linked list is \n");
+    display(first);
+
+    printf("\n");
+
+    printf("The sum of elements is : %d", sumOfLinkedList(first));
+
+    printf("\n");
+
+    printf("The sum of elements using recursive is : %d", recursiveSumOfLinkedList(first));
+    return 0;
+}
